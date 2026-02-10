a6d29f8042832b6c0167bb2ea3186fe943cdea7f
Nama Kamu <jessiska@student.umn.ac.id>
2026-02-10 20:06:11 +0700

    week02: implement selection logic for constructors

--- src/oop_00000114416_Jessiska/week02/Student.kt
+++ src/oop_00000114416_Jessiska/week02/Student.kt
@@ -14,7 +14,7 @@ class Student (
             println("LOG: Objek Student $name berhasil dialokasikan di Memory.")
         }
     }
-    constructor(name: String, nim: String) : this(name, nim, "Non-Matriculated"){
+    constructor(name: String, nim: String) : this(name, nim, gpa = 0.0,"Non-Matriculated"){
         println("LOG: Menggunakan constructor jalur umum (Tanpa Jurusan).")
     }
 }
