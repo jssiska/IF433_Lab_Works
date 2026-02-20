d75aa968915f24f2794639bd9cb4947938f5db6e
Nama Kamu <jessiska@student.umn.ac.id>
2026-02-20 21:15:34 +0700

    week03: cleanup code

--- src/oop_00000114416_Jessiska/week03/Main.kt
+++ src/oop_00000114416_Jessiska/week03/Main.kt
@@ -2,16 +2,11 @@ package oop_00000114416_Jessiska.week03
 
 fun main() {
     val e = Employee("Budi")
-
-    //1. Test Validasi Salary
-    e.salary = -1000 // Harusnya print error
+    e.salary = -1000
     e.salary = 5000000
     println("Gaji: ${e.salary}")
 
-    //2. Test Encapsulation
     e.increasePerformance()
-    //e.performanceRating = 5 // Coba uncomment, pasti Merah (Error)
-
-    //3. Test Computed Property
+    //e.performanceRating = 5
     println("Pajak yang harus dibayar: ${e.tax}")
 }
