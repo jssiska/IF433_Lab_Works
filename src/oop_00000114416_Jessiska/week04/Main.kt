79f28dd67ef822ba463a4c18d2d951d14347cd46
Nama Kamu <jessiska@student.umn.ac.id>
2026-02-24 15:33:25 +0700

    week04: test Vehicle and Car in main

---
+++ src/oop_00000114416_Jessiska/week04/Main.kt
@@ -0,0 +1,14 @@
+package oop_00000114416_Jessiska.week04
+
+fun main() {
+    println("--- Testing Vehicle ---")
+    val generalVehicle = Vehicle("Sepeda Onthel")
+    generalVehicle.honk()
+    generalVehicle.accelerate()
+
+    println("\n--- Testing Car ---")
+    val myCar = Car("Toyota", 4)
+    myCar.openTruck()
+    myCar.honk()
+    myCar.accelerate()
+}
