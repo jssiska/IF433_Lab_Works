c828bc192c62579fdbb22313f4fee12c5d61ea4a
Nama Kamu <jessiska@student.umn.ac.id>
2026-03-03 23:31:38 +0700

    week05: (task 2) implement payment polymorphism

--- src/oop_00000114416_Jessiska/week05/Main.kt
+++ src/oop_00000114416_Jessiska/week05/Main.kt
@@ -26,4 +26,12 @@ fun main() {
     println("Luas Persegi (4): ${mathHelper.hitungLuas(4)}")
     println("Luas Persegi Panjang (4 x 5): ${mathHelper.hitungLuas(4, 5)}")
     println("Luas Lingkaran (r = 7.0): ${mathHelper.hitungLuas(7.0)}")
+
+    val eWallet = EWallet(accountName = "Budi", balance = 50000.0)
+    val creditCard = CreditCard(accountName = "Andi", limit = 100000.0)
+    println()
+    val paymentMethods: List<PaymentMethod> = listOf(eWallet, creditCard)
+    for (method in paymentMethods) {
+        method.processPayment(75000.0)
+    }
 }
