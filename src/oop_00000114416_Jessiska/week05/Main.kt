7747c5ba723d7464022b74e2d73f8d8650995fcc
Nama Kamu <jessiska@student.umn.ac.id>
2026-03-03 23:35:27 +0700

    week05: (task 2) add smart casting for EWallet topUp recovery

--- src/oop_00000114416_Jessiska/week05/Main.kt
+++ src/oop_00000114416_Jessiska/week05/Main.kt
@@ -33,5 +33,9 @@ fun main() {
     val paymentMethods: List<PaymentMethod> = listOf(eWallet, creditCard)
     for (method in paymentMethods) {
         method.processPayment(75000.0)
+        if (method is EWallet) {
+            method.topUp(50000.0)
+            method.processPayment(75000.0)
+        }
     }
 }
