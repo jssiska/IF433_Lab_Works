95874d87a7b7eccb63faabebf1e116684d881ec0
Nama Kamu <jessiska@student.umn.ac.id>
2026-03-10 15:14:24 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000114416_Jessiska/week06/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000114416_Jessiska.week06
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.")}
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.")}
+}
