04288f78f44b159905e0705fce69b584118f2670
Nama Kamu <jessiska@student.umn.ac.id>
2026-03-10 22:29:53 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000114416_Jessiska/week06/MainTugas.kt
+++ src/oop_00000114416_Jessiska/week06/MainTugas.kt
@@ -4,4 +4,12 @@ fun main() {
     val lamp = SmartLamp("l", "Ruang Tamu")
     val speaker = SmartSpeaker("s", "Google Nest Dapur")
     val cctv = SmartCCTV("c", "Ezviz Garasi")
+    val hub = SmartHomeHub()
+
+    hub.addDevice(lamp)
+    hub.addDevice(speaker)
+    hub.addDevice(cctv)
+    hub.activateSecurityMode()
+    hub.turnOffAllSwitches()
+
 }
