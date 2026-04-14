eb6bbde8b1c86c112b9e2fa9ab47ebc4ea97a198
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-14 15:41:30 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000114416_Jessiska/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000114416_Jessiska.Week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
