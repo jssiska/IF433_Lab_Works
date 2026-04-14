2553e9ad561927870f048fbdc9a189a70d2ba4f7
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-14 15:22:43 +0700

    week08: implement java interop using git add .

--- src/oop_00000114416_Jessiska/Week08/Main.kt
+++ src/oop_00000114416_Jessiska/Week08/Main.kt
@@ -46,4 +46,9 @@ fun main(){
     } catch (e: IllegalArgumentException) {
         print(e.message)
     }
+
+    println("\n=== TEST JAVA INTEROP ===")
+    val javaResponse = LegacyJavaAPI.fetchServerStatus()
+    val statusLength = javaResponse!!.length
+    println("Status dari Java: $javaResponse (Length: $statusLength)")
 }
