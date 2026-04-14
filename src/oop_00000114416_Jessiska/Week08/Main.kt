5bbd87bd6474e1dcea1f480dad54e6d54c09f75c
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-14 14:53:40 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000114416_Jessiska/Week08/Main.kt
+++ src/oop_00000114416_Jessiska/Week08/Main.kt
@@ -19,4 +19,10 @@ fun main(){
     val mixedData: List<Any> = listOf(
         "Smartphone", 1500000, UserProfile("Andi", null), "Laptop", 4500000.0
     )
+    for(item in mixedData){
+        val text = item as? String
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
