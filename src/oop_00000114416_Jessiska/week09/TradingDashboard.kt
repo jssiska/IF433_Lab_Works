e71f87fe2d7c39f67f8798ac28a282c6765a33a8
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-21 15:55:12 +0700

    week09: (task) print top performers using forEach

--- src/oop_00000114416_Jessiska/week09/TradingDashboard.kt
+++ src/oop_00000114416_Jessiska/week09/TradingDashboard.kt
@@ -26,4 +26,8 @@ fun main(){
         }
 
     val uniquePairs = tradeHistory.map { it.pair }.toSet()
+    println("=== CRYPTO TRADING DASHBOARD ===")
+    topPerformersString.forEach {
+        println(it)
+    }
 }
