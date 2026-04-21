ea07794208ae1d9b04de606616d349a168c60b61
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-21 15:35:34 +0700

    week09: (task) populate mock trade history list

---
+++ src/oop_00000114416_Jessiska/week09/TradingDashboard.kt
@@ -0,0 +1,12 @@
+package oop_00000114416_Jessiska.week09
+
+fun main(){
+    val tradeHistory = listOf(
+        TradeLog("BTCUSDT", "LONG", 6, 12.5, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 5, -3.5, "CLOSED"),
+        TradeLog("BTCUSDT", "LONG", 20, 8.5, "OPEN"),
+        TradeLog("SOLUSDT", "LONG", 15, 5.0, "CLOSED"),
+        TradeLog("ETHUSDT", "SHORT", 10, -1.5, "OPEN"),
+        TradeLog("XRPUSDT", "LONG", 8, 20.0, "OPEN")
+    )
+}
