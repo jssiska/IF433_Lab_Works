841193a8010e66bcd68be715e45b76587d2251ab
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-28 15:18:25 +0700

    week10: implement PairBox in main function

--- src/oop_00000114416_Jessiska/week10/Main.kt
+++ src/oop_00000114416_Jessiska/week10/Main.kt
@@ -6,4 +6,8 @@ fun main(){
     val stringBox = Box("Generic in Kotlin")
     println("Isi intBox: ${intBox.value}")
     println("Isi stringBox: ${stringBox.value}")
+
+    println("\n=== TEST MULTIPLE PARAMETERS ===")
+    val itemPrice = PairBox("Bitcoin", 65000)
+    println("Aset: ${itemPrice.key}, Harga: ${itemPrice.value} USD")
 }
