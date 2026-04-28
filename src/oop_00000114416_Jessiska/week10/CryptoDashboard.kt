f3e8d46f45070683c994747ad56e12186d63b90f
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-28 15:47:02 +0700

    week10: (task) populate coin repository

--- src/oop_00000114416_Jessiska/week10/CryptoDashboard.kt
+++ src/oop_00000114416_Jessiska/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000114416_Jessiska.week10
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 5.0))
+    coinRepo.add(Coin("ETH", 10.0))
+    coinRepo.add(Coin("USDT", 100.0))
 }
