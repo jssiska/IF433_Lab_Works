eaa747cb03dd711a4813a1fd768f09d53df96823
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-28 15:47:54 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_00000114416_Jessiska/week10/CryptoDashboard.kt
+++ src/oop_00000114416_Jessiska/week10/CryptoDashboard.kt
@@ -5,4 +5,6 @@ fun main() {
     coinRepo.add(Coin("BTC", 5.0))
     coinRepo.add(Coin("ETH", 10.0))
     coinRepo.add(Coin("USDT", 100.0))
+
+    val response = ApiResponse("200 OK", coinRepo.getAll())
 }
