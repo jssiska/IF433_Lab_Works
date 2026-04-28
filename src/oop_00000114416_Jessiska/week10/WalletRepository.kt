a5004db0994bd3491fbad57e553ec04a908a9709
Nama Kamu <jessiska@student.umn.ac.id>
2026-04-28 15:36:19 +0700

    week10: (task) implement generic WalletRepository

---
+++ src/oop_00000114416_Jessiska/week10/WalletRepository.kt
@@ -0,0 +1,11 @@
+package oop_00000114416_Jessiska.week10
+
+class WalletRepository<T> {
+    private val items = mutableListOf<T>()
+    fun add(item: T) {
+        items.add(item)
+    }
+    fun getAll(): List<T> {
+        return items
+    }
+}
