e348842f780db9854d0c41ea2bba89e450ed224c
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-05 15:12:44 +0700

    week11: (task) create extension function for SmartDevice

--- src/oop_00000114416_Jessiska/week11/SmartHomeModels.kt
+++ src/oop_00000114416_Jessiska/week11/SmartHomeModels.kt
@@ -1,3 +1,9 @@
 package oop_00000114416_Jessiska.week11
 
 data class SmartDevice(var name: String, var category: String, var isOnline: Boolean = false, var powerLoad: Int = 0)
+
+fun SmartDevice.diagnose(): String {
+    return "[DIAGNOSTIK] $name | Kategori: $category | Status: ${
+        if (isOnline) "Online" else "Offline"
+    } | Daya: $powerLoad Watt"
+}
