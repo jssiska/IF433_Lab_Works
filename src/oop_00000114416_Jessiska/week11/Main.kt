3e7ed55a2b2c1c92c257259d30c76767b79413b5
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-05 14:55:44 +0700

    week11: implement run function for object computation

--- src/oop_00000114416_Jessiska/week11/Main.kt
+++ src/oop_00000114416_Jessiska/week11/Main.kt
@@ -6,4 +6,11 @@ fun main(){
     println("Hi".repeatTimes(3))
     val text:String? = null
     println("Apakah null/empty? ${text.isNullOrEmptyCustom()}")
+
+    println("\n=== TEST RUN FUNCTION ===")
+    val result = "Kotlin".run {
+        println("Memproses kata: ${this}")
+        length * 2
+    }
+    println("Hasil kalkulasi run: $result")
 }
