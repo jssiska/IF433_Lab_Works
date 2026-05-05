3452b1a4288c674c5ac31a123ff9e0a3abc5abc4
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-05 15:16:03 +0700

    week11: (task) configure Ezviz camera with also

--- src/oop_00000114416_Jessiska/week11/SmartHomeDashboard.kt
+++ src/oop_00000114416_Jessiska/week11/SmartHomeDashboard.kt
@@ -9,4 +9,13 @@ fun main() {
     }.also {
         homeDevices.add(it)
     }
+
+    SmartDevice("Ezviz Outdoor", "Camera").apply {
+            isOnline = true
+            powerLoad = 5
+        }
+        .also {
+            println("(LOG) Kamera terhubung")
+            homeDevices.add(it)
+        }
 }
