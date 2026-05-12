1912919fd32dc37dda3ba531b3d18a73844cec39
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-12 14:55:06 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000114416_Jessiska/week12/SmartFeeder.kt
+++ src/oop_00000114416_Jessiska/week12/SmartFeeder.kt
@@ -41,5 +41,8 @@ fun main(){
             availableGram = 1000,
             isJammed = false
         )
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
