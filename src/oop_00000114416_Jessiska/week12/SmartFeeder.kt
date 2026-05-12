7a60ae9928a0c980f8658d9e865f6a52d79c72e0
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-12 14:42:13 +0700

    week12: (task) implement dispenseKibble function

---
+++ src/oop_00000114416_Jessiska/week12/SmartFeeder.kt
@@ -0,0 +1,8 @@
+package oop_00000114416_Jessiska.week12
+
+fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean): Int{
+    require(requestedGram>0) {
+        "Porsi kibble harus lebih dari 0 gr"
+    }
+    return availableGram
+}
