df42032551113963d5772fce92cd6a46955348e2
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-19 15:14:12 +0700

    week13: (task) implement fromCsvTrade with split

--- src/oop_00000114416_Jessiska/week13/CryptoTradeLogger.kt
+++ src/oop_00000114416_Jessiska/week13/CryptoTradeLogger.kt
@@ -3,3 +3,8 @@ package oop_00000114416_Jessiska.week13
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
 
 fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
+
+fun fromCsvTrade(line: String): TradeRecord? {
+    val parts = line.split(",")
+    return TradeRecord(parts[0].toInt(),parts[1],parts[2],parts[3].toDouble(),parts[4].toDouble())
+}
