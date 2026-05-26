9c6864b41dacaf75eaf5679e800738b59047477a
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-26 13:34:17 +0700

    week14: implement basic writeText function

--- src/oop_00000114416_Jessiska/week14/SrpViolation.kt
+++ src/oop_00000114416_Jessiska/week14/SrpViolation.kt
@@ -1,2 +1,15 @@
 package oop_00000114416_Jessiska.week14
 
+data class User(val name: String, val email: String, val age: Int)
+
+class UserManager{
+    fun validateUser(user: User):Boolean {
+        return user.email.contains("@") && user.age >= 18
+    }
+    fun saveUserToDatabase(user: User) {
+        println("INSERT INTO user VALUES ('${user.name}', ${user.email}')")
+    }
+    fun sendWelcomeEmail(user: User) {
+        println("Sending email to: ${user.email}")
+    }
+}
