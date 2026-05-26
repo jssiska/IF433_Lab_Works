016e46af7350fa252c44a885d80c0f5897f7574e
Nama Kamu <jessiska@student.umn.ac.id>
2026-05-26 14:36:04 +0700

    week14: create DiscountStrategy interface and implementations

---
+++ src/oop_00000114416_Jessiska/week14/OcpRefactored.kt
@@ -0,0 +1,13 @@
+package oop_00000114416_Jessiska.week14
+
+interface DiscountStrategy{
+    fun apply(price: Double):Double
+}
+
+class StudentDiscount: DiscountStrategy{
+    override fun apply(price: Double) = price * 0.80
+}
+
+class MemberDiscount: DiscountStrategy{
+    override fun apply(price: Double) = price * 0.85
+}
